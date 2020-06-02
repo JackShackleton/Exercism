ecd6cdbb1bfa71274c29fceee0bea2808dc8d8a7
Christopher <christopher.pacific@gmail.com>
2020-06-02 13:47:52 -0400

    Luhn 2

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -6,3 +6,14 @@ To get started with TDD, see the `README.md` file in your
 `ruby/luhn` directory.
 =end
 
+class Luhn
+  def self.valid?(num)
+    if num.length < 2
+      return false
+    else
+      return true
+      num = num.gsub(/\s+/, "").reverse
+      num.values_at(* num.each_index.select {|i| i.even?})
+    end
+  end
+end
